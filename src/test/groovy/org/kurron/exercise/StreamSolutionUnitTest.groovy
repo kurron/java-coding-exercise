e1373f66feec504f7a3741587ae4dbea33b24d4b
Ron Kurr <kurr@kurron.org>
2018-02-24 13:09:16 -0500

    Cleaning up.

--- src/test/groovy/org/kurron/exercise/StreamSolutionUnitTest.groovy
+++ src/test/groovy/org/kurron/exercise/StreamSolutionUnitTest.groovy
@@ -21,5 +21,8 @@ class StreamSolutionUnitTest extends Specification {
         then: 'expected calculations are returned'
         def expected = ['A': 10, 'B': 2, 'C': 2, 'CD': 8, 'Z': 2]
         results == expected
+
+        and: 'dump the map just to visually verify the results'
+        println results
     }
 }
